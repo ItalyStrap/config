e14f0e61ea2d23d61d67b6c4013411a0d10bac38
overclokk <info@overclokk.net>
2019-12-25 20:39:50 +0100

    Added Filtered Config

---
+++ src/Config/FilteredConfig.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace ItalyStrap\Config;
+
+/**
+ * Class FilteredConfig
+ * @package ItalyStrap\Config
+ * @credits https://github.com/TypistTech/wp-option-store/blob/master/src/FilteredOptionStore.php
+ */
+class FilteredConfig extends Config
+{
+	/**
+	 * Option getter.
+	 *
+	 * @param string $key
+	 * @param string $default Name of option to retrieve.
+	 *
+	 * @return mixed|null Null if option not exists or its value is actually null.
+	 */
+	public function get( string $key, $default = null ) {
+		return apply_filters(
+			$this->filterName( $key ),
+			parent::get( $key, $default )
+		);
+	}
+	/**
+	 * Normalize option name and key to snake_case filter tag.
+	 *
+	 * @param string $default Name of option to retrieve.
+	 *                           Expected to not be SQL-escaped.
+	 *
+	 * @return string
+	 */
+	private function filterName( string $default ): string {
+		return \strtolower( $default );
+	}
+}
---
+++ src/Config/InvalidDataException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace ItalyStrap\View\Exceptions;
+
+
+class InvalidDataException extends \InvalidArgumentException
+{
+
+}
