298f1e829796ce14b7190fab2f38f5ae8cdecf68
Enea <info@overclokk.net>
2020-08-28 19:55:26 +0200

    Revert Config_Interface for BC

--- legacy/Config_Interface.php
+++ legacy/Config_Interface.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace ItalyStrap\Config;
 
+if ( \interface_exists( '\ItalyStrap\Config\Config_Interface' ) )
+	return;
+
 /**
  * Interface Config_Interface
  * @package ItalyStrap\Config
