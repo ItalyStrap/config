cc9674dba4a9939985d456096476a786075c0fb6
Enea <info@overclokk.net>
2022-09-01 07:40:06 +0200

    Fix old bench
    * New Bench files
    * Benchmark workflow
    * Test workflow

---
+++ tests/Benchmark/ConfigBench.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace ItalyStrap\Tests\Benchmark;
+
+use ItalyStrap\Config\Config;
+
+/**
+ * @BeforeMethods({"setUp"})
+ */
+final class ConfigBench {
+
+	private Config $config;
+
+	public function setUp(): void {
+		$this->config = new Config();
+	}
+
+	public function benchAdd() {
+		$this->config->add('index', 'value');
+	}
+
+	public function benchRemove() {
+		$this->config->remove(['index']);
+	}
+
+	public function benchToArray() {
+		$this->config->toArray();
+	}
+
+	public function benchToJson() {
+		$this->config->toJson();
+	}
+}
